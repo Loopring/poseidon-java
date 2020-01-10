bbd54153c12cc60b1625f76a65dae39824ceac19
yue.wang <yue.wang@loopring>
2020-01-10 17:10:33 +0800

    Add encode/decode function pair to make js compatible
    Signed-off-by: yue.wang <yue.wang@loopring>

--- src/main/java/com/loopring/utils/BigIntLittleEndianEncoding.java
+++ src/main/java/com/loopring/utils/BigIntLittleEndianEncoding.java
@@ -26,6 +26,11 @@ public class BigIntLittleEndianEncoding {
         return this;
     }
 
+    /*
+     * encode a BigInteger to le buffer
+     *     i.e. 01 == 0x01 => [1, 0, ...{32}..., 0]
+     *          17 == 0x12 => [2, 1, ...{32}..., 0]
+     */
     public byte[] encode(BigInteger x) {
         assert (!isNegative(x));
 
@@ -42,6 +47,51 @@ public class BigIntLittleEndianEncoding {
 
     public BigInteger decode(byte[] in) {
         return toBigInteger(in);
+    }
+
+    /*
+     * encode a BigInteger String to JS compatible le buffer String, because JS lib uses
+     * this method to format a leBuf and transfers which to bigint.
+     *     i.e. 01 == "01" => String([0, ...{32}..., 0, 1])
+     *          17 == "17" => String([0, ...{32}..., 1, 7])
+     */
+    public String encodeJsBigInt(String bigIntStr) {
+        byte[] in = bigIntStr.getBytes();
+        assert (in.length <= byteLength);
+        if (in.length > byteLength) {
+            return new String("");
+        }
+
+        byte[] out = new byte[byteLength];
+        for (int i = 0; i < in.length; i++) {
+            int c = in[i];
+            assert(c >= '0' && c <= '9');
+            if (c < '0' || c > '9') {
+                return new String("");
+            }
+            out[byteLength - in.length + i] = (byte)(c);
+        }
+
+        for (int i = 0; i < byteLength - in.length; i++) {
+            out[i] = '0';
+        }
+
+        return new String(out);
+    }
+
+    public BigInteger decodeJsBigInt(String jsLeBigIntString) {
+        byte[] in = jsLeBigIntString.getBytes();
+        BigInteger result = BigInteger.ZERO;
+        for (int i = 0; i < in.length; i++) {
+            int c = in[i];
+            assert(c >= '0' && c <= '9');
+            if (c < '0' || c > '9') {
+                return BigInteger.ZERO;
+            }
+            BigInteger bigIntC = BigInteger.valueOf(c);
+            result = result.add(bigIntC.shiftLeft(i * 8));
+        }
+        return result;
     }
 
     private BigInteger toBigInteger(byte[] in) {
